f86d0984f2461b6ed4b2ca794a0b45ef208a126e
zephsinx <zephsinx@gmail.com>
2025-11-03 20:38:29 -0800

    Add Google Drive service for cloud backups

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,6 +4,7 @@
 
 export { AssetService } from "./assetService";
 export { CacheService, CACHE_KEYS } from "./cacheService";
+export { GoogleDriveService } from "./googleDriveService";
 export { ImportExportService } from "./importExportService";
 export { SettingsService } from "./settingsService";
 export { SpawnProfileService } from "./spawnProfileService";
